9fd9387e1d1ada244238082c9d76740b37a281bc
danielbarbosadev <daniel.barbosa5076@gmail.com>
2026-05-24 23:42:32 -0300

    :books:docs: Adiciona arquivo .h para o Projetor

---
+++ include/Projetor.h
@@ -0,0 +1,14 @@
+#ifndef PROJETOR_H
+#define PROJETOR_H
+
+#include <ArduinoJson.h>
+
+extern const char *comandoProjetor;
+extern const char PINO_BOTAO_BOOT;
+
+void verificarHandshakeProjetor(JsonDocument& doc);
+void configurarPinoBoot();
+void enviarComandoProjetor(const char* comandoProjetor);
+void loopComandoProjetor();
+
+#endif
