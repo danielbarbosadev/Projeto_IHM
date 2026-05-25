7a5fbd50b4e0b0df6e292d6289e53ddde5af93a2
danielbarbosadev <daniel.barbosa5076@gmail.com>
2026-05-25 18:01:22 -0300

    :sparkles:feat: Adiciona arquivos locais dos módulos

--- src/IHM.cpp
+++ src/IHM.cpp
@@ -1,9 +1,16 @@
 #include <Nextion.h>
 
+//* ======================
 //* ARQUIVOS LOCAIS
+//*=======================
 #include "WiFiManager.h"
 #include "MqttManager.h"
 #include "DebugManager.h"
 #include "secrets.h"
 #include "Enum.h"
 #include "Projetor.h"
+#include "Tela.h"
+#include "Televisao.h"
+#include "Lampadas.h"
+#include "ArCondicionado.h"
+#include "Temperatura.h"
