882da493d44f760a599c43cc1a84c4028c9c29ca
DANIEL DE LIMA BARBOSA <24381263880@info.local>
2026-05-25 10:40:05 -0300

    :books:docs: Cria arquivos .h e .cpp para IHM

---
+++ include/IHM.h
@@ -0,0 +1,4 @@
+#ifndef IMH_H
+#define IHM_H
+
+#endif
---
+++ src/IHM.cpp
@@ -0,0 +1,9 @@
+#include <Nextion.h>
+
+//* ARQUIVOS LOCAIS
+#include "WiFiManager.h"
+#include "MqttManager.h"
+#include "DebugManager.h"
+#include "secrets.h"
+#include "Enum.h"
+#include "Projetor.h"
