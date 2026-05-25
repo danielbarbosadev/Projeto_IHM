38c61161cbbbde001aef7de90a8bf88519e25dda
danielbarbosadev <daniel.barbosa5076@gmail.com>
2026-05-25 18:02:39 -0300

    :sparkles:feat: Adiciona funções de verificações de handshake dos módulos

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,28 @@
+//* ======================
 //* BIBLIOTECAS
+//*=======================
 #include <Arduino.h>
 #include <ArduinoJson.h>
 
+//* ======================
 //* ARQUIVOS LOCAIS
+//*=======================
 #include "WiFiManager.h"
 #include "MqttManager.h"
 #include "DebugManager.h"
 #include "secrets.h"
 #include "Enum.h"
 #include "Projetor.h"
+#include "Tela.h"
+#include "Televisao.h"
+#include "Lampadas.h"
+#include "ArCondicionado.h"
+#include "Temperatura.h"
+#include "IHM.h"
 
+//* ======================
 //* PROTÓTIPOS DAS FUNÇÕES
+//*=======================
 modulos identificarTopicos(const char *topico);
 void tratarMensagemRecebida(const char *, const String &);
 void tratarJsonHandshake(const String &, modulos);
@@ -79,93 +91,23 @@ void tratarJsonHandshake(const String &mensagem, modulos moduloRecebido)
     break;
 
   case TELA:
-  //TODO Implementar arquivo Tela.cpp
-    static bool handshakeTela;
-    if (doc["handshake"]["situacao"].is<bool>())
-    {
-      handshakeTela = doc["handshake"]["situacao"].as<bool>();
-
-      if (!handshakeTela)
-      {
-        debugErro("Falha ao encontrar comando, reenvie novamente");
-      }
-      else
-      {
-        debugInfo("Comando confirmado");
-      }
-    }
+    verificarHandshakeTela(doc);
     break;
 
   case TELEVISAO:
-  //TODO Implementar arquivo Televisao.cpp
-    static bool handshakeTelevisao;
-    if (doc["handshake"]["situacao"].is<bool>())
-    {
-      handshakeTelevisao = doc["handshake"]["situacao"].as<bool>();
-
-      if (!handshakeTelevisao)
-      {
-        debugErro("Falha no comando, reenvie denovo");
-      }
-      else
-      {
-        debugInfo("Comando confirmado");
-      }
-    }
+    verificarHandshakeTelevisao(doc);
     break;
 
   case LAMPADAS:
-  //TODO Implementar arquivo Lampadas.cpp
-    static bool handshakeLampadas;
-    if (doc["handshake"]["situacao"].is<bool>())
-    {
-      handshakeLampadas = doc["handshake"]["situacao"].as<bool>();
-
-      if (!handshakeLampadas)
-      {
-        debugErro("Falha no comando, reenvie denovo");
-      }
-      else
-      {
-        debugInfo("Comando confirmado");
-      }
-    }
+    verificarHandshakeLampadas(doc);
     break;
 
   case AR_CONDICIONADO:
-  //TODO Implementar arquivo Ar_Condicionado.cpp
-    static bool handshakeArCondicionado;
-    if (doc["handshake"]["situacao"].is<bool>())
-    {
-      handshakeArCondicionado = doc["handshake"]["situacao"].as<bool>();
-
-      if (!handshakeArCondicionado)
-      {
-        debugErro("Falha no comando, reenvie denovo");
-      }
-      else
-      {
-        debugInfo("Comando confirmado");
-      }
-    }
+    verificarHandshakeArCondicionado(doc);
     break;
 
   case TEMPERATURA:
-  //TODO Implementar arquivo Temperatura.cpp
-    static bool handshakeTemperatura;
-    if (doc["handshake"]["situacao"].is<bool>())
-    {
-      handshakeTemperatura = doc["handshake"]["situacao"].as<bool>();
-
-      if (!handshakeTemperatura)
-      {
-        debugErro("Falha no comando, reenvie denovo");
-      }
-      else
-      {
-        debugInfo("Comando confirmado");
-      }
-    }
+    verificarHandshakeTemperatura(doc);
     break;
 
     default:
