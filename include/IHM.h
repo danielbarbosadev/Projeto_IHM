619c475eb90bad17279155e57f95aef4d402d6da
DANIEL DE LIMA BARBOSA <24381263880@info.local>
2026-05-26 11:00:32 -0300

    :construction: Inicia configuração da IHM

--- include/IHM.h
+++ include/IHM.h
@@ -1,4 +1,20 @@
 #ifndef IMH_H
 #define IHM_H
 
+#include <Nextion.h>
+
+//* SIMPLE BUTTON 
+NexButton bntMenuInicial(1, 2, "BotaoInicial");
+NexButton btnMenuTelevisao(1, 1, "BotaoMenuTV");
+NexButton btnMenuLampadas(1, 2, "BotaoMenuLPD");
+NexButton btnMenuArCondicionado(1, 3, "BotaoMenuAC");
+NexButton btnMenuTemperatura(1, 4, "BotaoMenuTemp");
+NexButton btnMenuProjetorTela(1, 5, "BotaoMenuPT");
+
+void configurarInicializacaoNextion();
+void configurarEventosNextion();
+void configurarTelaArCondicionado();
+
+
+
 #endif
