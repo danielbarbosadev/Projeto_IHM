4117a897c2e69f0ef6812d3adeadf46f30486fb6
danielbarbosadev <daniel.barbosa5076@gmail.com>
2026-06-01 13:01:50 -0300

    :recycle:refactor: Altera nomenclatura

---
+++ include/Sensor.h
@@ -0,0 +1,8 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+
+#include <Arduino.h>
+void verificarHandshakeSensor(const String& mensagem);
+void enviarComandoSensor();
+
+#endif
---
+++ src/Sensor.cpp
@@ -0,0 +1,56 @@
+#include <ArduinoJson.h>
+
+//* ======================
+//* ARQUIVOS LOCAIS
+//*=======================
+#include "MqttManager.h"
+#include "DebugManager.h"
+#include "Enum.h"
+#include "Sensor.h"
+
+
+void verificarHandshakeSensor(const String& mensagem)
+{
+    JsonDocument doc;
+
+    DeserializationError erro = deserializeJson(doc, mensagem);
+
+    if (erro)
+    {
+        debugErro("Erro ao interpretar JSON");
+        debugErro(erro.c_str());
+        return;
+    }
+
+    if (doc["handshake"]["situacao"].isNull())
+    {
+        debugErro("Resposta da situação não foi enviada no JSON");
+        return;
+    }
+    
+    if (doc["handshake"]["situacao"].is<bool>())
+    {
+        bool handshake = doc["handshake"]["situacao"].as<bool>();
+
+        if (!handshake)
+        {
+            debugErro("Falha no comando, reenvie denovo");
+        }
+        else
+        {
+            debugInfo("Comando confirmado");
+        }
+    }
+}
+
+void enviarComandoSensor()
+{
+    bool enviarDados = true;
+    JsonDocument doc;
+    String mensagem;
+
+    doc["Sensor"]["comando"] = enviarDados;
+    serializeJson(doc, mensagem);
+    publicarMensagemNoTopico(TOPICO_SENSOR, mensagem.c_str());
+    enviarDados = false;
+}
