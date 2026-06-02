d0454dc532d25e850fb26f5705cea53a5c3cba6d
DANIEL DE LIMA BARBOSA <24381263880@info.local>
2026-06-02 10:52:57 -0300

    :recycle:refactor: Remove variavel errada do .h

--- include/Tela.h
+++ include/Tela.h
@@ -1,7 +1,7 @@
 #ifndef TELA_H
 #define TELA_H
 
-extern char telaEscolhida[20] = "";
+extern char telaEscolhida[32];
 
 void verificarHandshakeTela(const String& mensagem);
 void enviarComandoTela(uint32_t comandoTela);
