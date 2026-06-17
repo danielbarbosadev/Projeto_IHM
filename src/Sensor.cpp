5cf746d50b0d8ab6942a855b2a43a1cff82121c3
danielbarbosadev <daniel.barbosa5076@gmail.com>
2026-06-17 14:13:40 -0300

    ✨feat: Adiciona funções para verificação e envio de texto com os dados dos sensores

--- src/Sensor.cpp
+++ src/Sensor.cpp
@@ -10,7 +10,14 @@
 #include "Sensor.h"
 
 
-void verificarMensagemSensor(const String& mensagem)
+#define SENSOR_ERRO -999
+
+static bool leituraComErro(float valor)
+{
+    return valor <= SENSOR_ERRO;
+}
+
+void verificarMensagemSensor(const String &mensagem)
 {
     JsonDocument doc;
 
@@ -18,47 +25,64 @@ void verificarMensagemSensor(const String& mensagem)
 
     if (erro)
     {
+        mostrarMensagemErro();
         debugErro("Erro ao interpretar JSON");
         debugErro(erro.c_str());
         return;
     }
 
-    if (doc["statusComando"]["comando"].isNull())
+    if (doc["sensores"].isNull())
     {
-        debugErro("Resposta do comando não foi enviada no JSON");
+        mostrarMensagemErro();
         return;
     }
 
-    if (doc["statusComando"]["situacao"].isNull())
-    {
-        debugErro("Resposta da situação não foi enviada no JSON");
-        return;
+    JsonObject sensores = doc["sensores"];
+
+    if (!sensores["temperatura"].isNull())
+ 	    {
+        float temperatura = sensores["temperatura"].as<float>();
+
+        if (leituraComErro(temperatura))
+            txtTemperatura.texto("Erro").visivel(true);
+        else
+            txtTemperatura.texto(char(temperatura) + " C").visivel(true);
     }
-    
-    if (doc["statusComando"]["situacao"].is<const char*>())
+
+    if (!sensores["umidade"].isNull())
     {
-       const char* situacao =  doc["statusComando"]["situacao"].as<const char*>();
+        float umidade = sensores["umidade"].as<float>();
+
+        if (leituraComErro(umidade))
+            txtUmidade.texto("Erro").visivel(true);
+        else
+            txtUmidade.texto(char(umidade) + "%").visivel(true);
     }
-    //TODO: COLOCAR JSON CERTO
-    //TODO: COLOCAR INFORMAÇÕES DO JSON NAS VARIÁVEIS   
-    //TODO: ENVIAR PARA A FUNÇÃO mostrarDadosSensor();
+
+    if (!sensores["som"].isNull())
+    {
+        float som = sensores["som"].as<float>();
+
+        if (leituraComErro(som))
+            txtSom.texto("Erro").visivel(true);
+        else
+            txtSom.texto(char(som) + " dB").visivel(true);
+    }
 }
 
 void enviarComandoSensor()
 {
-    bool enviarDados = true;
     JsonDocument doc;
     String mensagem;
 
-    doc["sensor"]["comando"] = enviarDados;
+    doc["sensores"]["comando"] = true;
     serializeJson(doc, mensagem);
     publicarMensagemNoTopico(TOPICO_SENSOR, mensagem.c_str());
-    enviarDados = false;
 }
 
-void mostrarDadosSensor(const char* temperatura, const char* umidade, const char* som)
+void mostrarMensagemErro()
 {
-    textoTemperatura.texto(temperatura);
-    textoUmidade.texto(umidade);
-    textoSom.texto(som);
+    txtTemperatura.texto("Erro").visivel(true);
+    txtUmidade.texto("Erro").visivel(true);
+    txtSom.texto("Erro").visivel(true);
 }
